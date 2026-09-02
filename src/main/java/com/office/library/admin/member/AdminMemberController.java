034f0e6e939f5b2a42fee66c836a8cbfca8cf578
hohasic <hohasic@gmail.com>
2026-09-02 09:00:45 +0900

    20260902

--- src/main/java/com/office/library/admin/member/AdminMemberController.java
+++ src/main/java/com/office/library/admin/member/AdminMemberController.java
@@ -5,11 +5,9 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -164,14 +162,3 @@ public class AdminMemberController {
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
